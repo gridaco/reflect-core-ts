a78f7525e2df8370c78e5400e823e313898eb493
softmarshmallow <woojoo@softmarshmallow.com>
2020-12-07 21:07:25 +0900

    added border manifest placeholder

---
+++ lib/border/README.md
@@ -0,0 +1,11 @@
+# Reflect#Border
+> reflect's universal border data manifest
+
+## [css border](https://developer.mozilla.org/en-US/docs/Web/CSS/border)
+
+- border - https://developer.mozilla.org/en-US/docs/Web/CSS/border
+
+## [flutter border](https://api.flutter.dev/flutter/painting/Border-class.html)
+
+- border - https://api.flutter.dev/flutter/painting/Border-class.html
+- border side - https://api.flutter.dev/flutter/painting/BorderSide-class.html
--- lib/box-shadow/box-shadow.manifest.ts
+++ lib/box-shadow/box-shadow.manifest.ts
@@ -2,9 +2,15 @@ import { Color } from "../color";
 import { Offset } from "../ui/offset";
 
 /**
+ * Universal shadow data interface
+ * 
  * [flutter#BoxShadow](https://api.flutter.dev/flutter/painting/BoxShadow-class.html)
+ * 
  * [css#BoxShadow](https://developer.mozilla.org/en-US/docs/Web/CSS/box-shadow)
+ * 
  * [figma#ShadowEffect](https://www.figma.com/plugin-docs/api/Effect/#shadoweffect)
+ * 
+ * [konva#Rect.shadow](https://konvajs.org/api/Konva.Rect.html)
  */
 export interface BoxShadowManifest {
     color: Color
