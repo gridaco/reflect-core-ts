64acf0f0eb4513c886e4d69cc0276c9e41a0fd72
softmarshmallow <woojoo@softmarshmallow.com>
2020-12-25 18:19:47 +0900

    slots reservation

---
+++ lib/button.base/README.md
@@ -0,0 +1,5 @@
+# Button Base; Reflect button - button.base
+
+> definition of reflect button's base manifest.
+
+learn more at [reflect docs on notion](https://reflect.bridged.xyz/Button-ebdf777a479f4d1bb3f7c6db7672117a)
--- lib/button/button.manifest.ts
+++ lib/button/button.manifest.ts
@@ -1,13 +1,18 @@
-export type ButtonVariant =
-    "FlatText" |
-    "FlatTextIcon" |
-    "RaisedText" |
-    "RaisedTextIcon" |
-    "OutlinedText" |
-    "OutlinedTextIcon" |
-    "Icon"
+export enum ButtonVariant {
+    flatText = "FlatText",
+    flatTextIcon = "FlatTextIcon",
+    raisedText = "RaisedText",
+    raisedTextIcon = "RaisedTextIcon",
+    outlinedText = "OutlinedText",
+    outlinedTextIcon = "OutlinedTextIcon",
+    icon = "Icon",
+    custom = "Custom"
+}
 
 
+/**
+ * relfect's button manifest
+ */
 export interface ButtonManifest<B, T, I> {
     base: B
     text?: T | null
