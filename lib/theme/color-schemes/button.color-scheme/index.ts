6f50549d8b7e66239831b4106e9afe6861073ab4
softmarshmallow <woojoo@softmarshmallow.com>
2020-12-26 18:39:03 +0900

    added theme, and color scheme folder

---
+++ lib/theme/color-schemes/button.color-scheme/index.ts
@@ -0,0 +1,10 @@
+import { Color } from "../../../color/color.manifest";
+
+/**
+ * reflect button color scheme manifest
+ */
+export interface ButtonColorScheme {
+    base: Color
+    text: Color,
+    border?: Color
+}
