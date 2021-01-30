13fafdda6b1164ee10bf8a49e8772ed217b0ca91
softmarshmallow <woojoo@softmarshmallow.com>
2021-01-31 04:12:28 +0900

    added ant-design and bridged as defaut icon provider host

--- lib/icon/icon.config.ts
+++ lib/icon/icon.config.ts
@@ -2,5 +2,5 @@ export interface IconConfig {
     default_size: string | number;
     variant: "outlined" | "twotone" | "default" | "sharp" | string;
     family?: string;
-    host: "material" | "reflect" | string;
+    host: "material" | "reflect" | "ant-design" | "bridged" | string;
 }
