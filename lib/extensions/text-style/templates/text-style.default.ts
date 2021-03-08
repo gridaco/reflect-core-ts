01e2f8ba1ada3138036af293a5e8bdbf62b66292
softmarshmallow <woojoo@softmarshmallow.com>
2021-03-08 16:34:42 +0900

    add textstyle defaults

---
+++ lib/extensions/text-style/templates/text-style.default.ts
@@ -0,0 +1,15 @@
+export const REFLECT_DEFAULT_FONT_SIZE = 14;
+export const REFLECT_DEFAULT_FONT_WEIGHT_LIGHT = 300;
+export const REFLECT_DEFAULT_FONT_WEIGHT_REGULAR = 400;
+export const REFLECT_DEFAULT_FONT_WEIGHT_MEDIUM = 500;
+export const REFLECT_DEFAULT_FONT_WEIGHT_BOLD = 700;
+/**
+ * default font-size used by browsers is 16px.
+ */
+export const REFLECT_WEB_DEFAULT_HTLM_FONT_SIZE = 16;
+export const REFLECT_WEB_DEFAULT_FONT_FAMILY = [
+    "Roboto",
+    "Helvetica",
+    "Arial",
+    "sans-serif",
+];
