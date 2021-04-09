07c0c7a29a14366c70a1bb8a6010f22596ed1297
softmarshmallow <woojoo@softmarshmallow.com>
2021-04-09 21:13:43 +0900

    update ver, export color

--- lib/index.ts
+++ lib/index.ts
@@ -1,13 +1,15 @@
-export * from "./button"
-export * from "./text"
-export * from "./text-style"
-export * from "./text-align"
-export * from "./text-decoration"
-export * from "./icon"
-export * from "./image"
-export * from "./font-weight"
-export * from "./font-style"
-export * from "./layouts"
-export * from "./rect"
-export * from "./ui"
-export * as converters from "./converters"
+export * from "./button";
+export * from "./text";
+export * from "./text-style";
+export * from "./text-align";
+export * from "./text-decoration";
+export * from "./icon";
+export * from "./image";
+export * from "./font-weight";
+export * from "./font-style";
+export * from "./layouts";
+export * from "./rect";
+export * from "./ui";
+export * as color from "./color";
+
+export * as converters from "./converters";
