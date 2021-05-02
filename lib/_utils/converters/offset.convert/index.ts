bd8cb2f3ed30feed0d67746a49bd531ec0b3d082
softmarshmallow <woojoo@softmarshmallow.com>
2021-05-02 18:28:22 +0900

    fix broken import path

--- lib/_utils/converters/offset.convert/index.ts
+++ lib/_utils/converters/offset.convert/index.ts
@@ -1,11 +1,11 @@
-import { Offset } from "../../ui";
+import { Offset } from "../../../ui";
 
 export function convertOffsetToUniversal(offset?: Offset) {
     if (offset) {
         return {
             x: offset.dx,
-            y: offset.dy
-        }
+            y: offset.dy,
+        };
     }
-    return undefined
+    return undefined;
 }
