b3a6db539bf7de6c92755090339d6549fd175d70
softmarshmallow <woojoo@softmarshmallow.com>
2021-05-02 20:34:50 +0900

    add empty stack as layout

--- lib/layouts/index.ts
+++ lib/layouts/index.ts
@@ -1,1 +1,15 @@
-export * from "./flex"
+export * from "./flex";
+export * from "./row";
+export * from "./column";
+
+import type { Row } from "./row";
+import type { Flex } from "./flex";
+import type { Column } from "./column";
+import type { Stack } from "./stack";
+
+/**
+ * the type alias that can represent a layout
+ *
+ * Row | Column | Flex | Stack
+ */
+export type LayoutRepresntatives = Row | Column | Flex | Stack;
