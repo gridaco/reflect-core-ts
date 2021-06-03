266caf8496be4311d19e1bca6c3ef2a7d8723a6a
softmarshmallow <woojoo@softmarshmallow.com>
2021-06-03 11:16:15 +0900

    add widget key typings

--- lib/widget/widget.ts
+++ lib/widget/widget.ts
@@ -1,9 +1,18 @@
+/**
+ * the default widget key interface. infered from design source.
+ */
+interface WidgetKey {
+    id: string;
+    originName: string;
+}
+type WidgetKeyLike = WidgetKey;
+
 export class Widget {
     readonly _type: string;
-    key?: string;
+    key?: WidgetKeyLike;
     children: Widget[];
 
-    constructor(p?: { key: string }) {
+    constructor(p?: { key: WidgetKeyLike }) {
         this.key = p?.key;
     }
 }
