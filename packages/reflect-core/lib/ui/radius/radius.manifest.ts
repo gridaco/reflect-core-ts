7a0d2ed39033fb27e55f098c8585002d0691f566
softmarshmallow <woojoo@softmarshmallow.com>
2021-09-15 12:48:39 +0900

    add border radius as default property for container

--- packages/reflect-core/lib/ui/radius/radius.manifest.ts
+++ packages/reflect-core/lib/ui/radius/radius.manifest.ts
@@ -1,14 +1,32 @@
 /**
  * [flutter#Radius.circular](https://api.flutter.dev/flutter/dart-ui/Radius/Radius.circular.html)
  */
-type ICircularRadius = number
+type ICircularRadius = number;
 
 /**
  * [flutter#Radius.elliptical](https://api.flutter.dev/flutter/dart-ui/Radius/Radius.elliptical.html)
  */
 interface IEllipticalRadius {
-    x: number
-    y: number
+    x: number;
+    y: number;
 }
 
-export type IRadius = ICircularRadius | IEllipticalRadius
+export type IRadius = ICircularRadius | IEllipticalRadius;
+
+export function isCircularRadius(radius: IRadius): radius is ICircularRadius {
+    return typeof radius === "number";
+}
+
+export function isEllipticalRadius(
+    radius: IRadius
+): radius is IEllipticalRadius {
+    return (
+        typeof radius === "object" &&
+        typeof radius.x === "number" &&
+        typeof radius.y === "number"
+    );
+}
+
+export function isRadius(radius: IRadius): radius is IRadius {
+    return isCircularRadius(radius) || isEllipticalRadius(radius);
+}
