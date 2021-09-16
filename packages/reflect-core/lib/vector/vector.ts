8c657f944c9d1660cd8faa7ba24db96a805c5a1e
softmarshmallow <woojoo@softmarshmallow.com>
2021-09-16 18:34:31 +0900

    add basic properties to vector widget

--- packages/reflect-core/lib/vector/vector.ts
+++ packages/reflect-core/lib/vector/vector.ts
@@ -2,8 +2,24 @@ import { Widget, WidgetKey } from "../widget";
 
 export class VectorWidget extends Widget {
     readonly data: string;
-    constructor(p: { key: WidgetKey; data: string }) {
+    constructor(p: {
+        key: WidgetKey;
+        width: number;
+        height: number;
+
+        /**
+         * svg data
+         */
+        data: string;
+    }) {
         super(p);
+        // region general
+        this.width = p.width;
+        this.height = p.height;
+        //
+
+        // region svg related
         this.data = p.data;
+        // endregion
     }
 }
