b672c123e056ae058ef02d582bddbb44957be80c
softmarshmallow <woojoo@softmarshmallow.com>
2021-09-20 11:40:29 +0900

    update core image widget init

--- packages/reflect-core/lib/image/image.ts
+++ packages/reflect-core/lib/image/image.ts
@@ -1,11 +1,25 @@
 import { ImageManifest } from ".";
-import { Widget } from "../widget";
+import { Widget, WidgetKey } from "../widget";
 
 export class ImageWidget extends Widget implements ImageManifest {
     readonly src: string;
     readonly width: number;
     readonly height: number;
-    constructor() {
-        super();
+    constructor({
+        key,
+        src,
+        width,
+        height,
+    }: {
+        key: WidgetKey;
+        src: string;
+        width: number;
+        height: number;
+    }) {
+        super({ key });
+
+        this.src = src;
+        this.width = width;
+        this.height = height;
     }
 }
