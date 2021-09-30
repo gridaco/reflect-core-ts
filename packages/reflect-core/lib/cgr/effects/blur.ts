27f82724a64bf740279668d00725d8d1f1d9da90
You-j <ijyk3061@gmail.com>
2021-09-30 21:21:12 +0900

    add token: blurred

--- packages/reflect-core/lib/cgr/effects/blur.ts
+++ packages/reflect-core/lib/cgr/effects/blur.ts
@@ -6,23 +6,3 @@ export interface BlurEffect {
     readonly radius: number;
     readonly visible: boolean;
 }
-
-export class Blur extends Widget {
-    readonly _type = "BluerEffects";
-    readonly blur: BlurEffect[];
-    readonly child?: Widget;
-    constructor({
-        key,
-        blur,
-        child,
-    }: {
-        key: WidgetKey;
-        blur: BlurEffect[];
-        child: Widget;
-    }) {
-        super({ key });
-        assert(typeof blur, "blur is required");
-        this.blur = blur;
-        this.child = child;
-    }
-}
