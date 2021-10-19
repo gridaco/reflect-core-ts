fd8eea1662f0364327dd0f1763240af8035e1773
softmarshmallow <woojoo@softmarshmallow.com>
2021-10-19 17:02:01 +0900

    update `text` property name to `data` under text manifest

--- packages/reflect-core/lib/text/text.manifest.ts
+++ packages/reflect-core/lib/text/text.manifest.ts
@@ -6,24 +6,22 @@ import { TextStyleManifest } from "../text-style";
  * [flutter docs](https://api.flutter.dev/flutter/widgets/Text-class.html)
  */
 export interface TextManifest {
-    text: string
-    style: TextStyleManifest
-    overflow: TextOverflowManifest
+    data: string;
+    style: TextStyleManifest;
+    overflow: TextOverflowManifest;
 
     /**
      * [flutter Text#textAlign](https://api.flutter.dev/flutter/widgets/Text/textAlign.html)
      */
-    textAlign: TextAlignManifest
-
+    textAlign: TextAlignManifest;
 
     /**
      * this is not a default css nor flutter's text property, but this field exists on figma or other design-first tools.
      */
-    textAlignVertical: TextAlignVerticalManifest
-
+    textAlignVertical: TextAlignVerticalManifest;
 
     /**
      * this is not supported with css standard [related link here](https://stackoverflow.com/questions/55487695/react-native-text-component-using-number-of-lines)
      */
-    maxLines: number
+    maxLines: number;
 }
