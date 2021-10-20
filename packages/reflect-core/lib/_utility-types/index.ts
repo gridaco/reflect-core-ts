434cc4e5c7f31869d5557eb350639b5c641bcef8
softmarshmallow <woojoo@softmarshmallow.com>
2021-10-20 17:59:10 +0900

    update proxied type

--- packages/reflect-core/lib/_utility-types/index.ts
+++ packages/reflect-core/lib/_utility-types/index.ts
@@ -1,7 +1,11 @@
-export interface Accessor<T> {
-    key: string;
-    type: T;
-}
-export type Proxied<T> = Accessor<T> | T;
 export type Dynamic<T> = Proxied<T> | Rendered<T>;
 export type Rendered<T> = T;
+
+export interface IProxied {
+    readonly proxied: true;
+}
+
+export abstract class Proxied<T> implements IProxied {
+    readonly proxied: true = true;
+    constructor() {}
+}
