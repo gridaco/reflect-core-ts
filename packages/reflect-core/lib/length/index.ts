f0663b2aa0b76ace158fef054a67a520b01d94eb
softmarshmallow <woojoo@softmarshmallow.com>
2021-12-31 02:15:47 +0900

    add utility type checker func - `isPossibleDimensionLength`

--- packages/reflect-core/lib/length/index.ts
+++ packages/reflect-core/lib/length/index.ts
@@ -22,3 +22,22 @@ export type Operation = {
     left: DimensionLength;
     right: DimensionLength;
 };
+
+export function isPossibleDimensionLength(dl: DimensionLength | any): boolean {
+    if (typeof dl === "number") return true;
+    if (dl === "match-screen-size") return true;
+    if (typeof dl === "string") {
+        const matched =
+            /\\d+(?:\\.\\d+)?%/g.test(dl) || // %
+            /\\d+(?:\\.\\d+)?vh/g.test(dl) || // vh
+            /\\d+(?:\\.\\d+)?vw/g.test(dl); // vw
+        if (matched) return true;
+    }
+    // calc
+    if (typeof dl === "object") {
+        if (dl.type === "calc") {
+            return true;
+        }
+    }
+    return false;
+}
