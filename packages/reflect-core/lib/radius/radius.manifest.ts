b602343e8fdb8b22f1e32f68f2302b2143aba54b
softmarshmallow <woojoo@softmarshmallow.com>
2022-03-18 16:15:48 +0900

    add Radius utility class & constructor

--- packages/reflect-core/lib/radius/radius.manifest.ts
+++ packages/reflect-core/lib/radius/radius.manifest.ts
@@ -13,6 +13,33 @@ interface IEllipticalRadius {
 
 export type IRadius = ICircularRadius | IEllipticalRadius;
 
+export class Radius implements IEllipticalRadius {
+    public static circular(radius: number): Radius {
+        return new Radius(radius);
+    }
+
+    public static elliptical(x: number, y: number): Radius {
+        return new Radius(x, y);
+    }
+
+    constructor(private _x: number, private _y?: number) {}
+
+    public get isCircular(): boolean {
+        return (
+            this._x !== undefined &&
+            (this._x === this._y || this._y === undefined)
+        );
+    }
+
+    public get x(): number {
+        return this._x;
+    }
+
+    public get y(): number {
+        return this._y || this._x;
+    }
+}
+
 export function isCircularRadius(radius: IRadius): radius is ICircularRadius {
     return typeof radius === "number";
 }
