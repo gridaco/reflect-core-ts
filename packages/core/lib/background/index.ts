89f10b3dc3963bac93ec5868d9430f068210f6f5
softmarshmallow <woojoo@softmarshmallow.com>
2023-06-19 06:31:20 +0900

    add image background type

--- packages/core/lib/background/index.ts
+++ packages/core/lib/background/index.ts
@@ -1,12 +1,32 @@
 import type { Color } from "../color";
 import type { Gradient } from "../gradient";
+import type { ImageManifest } from "../image";
+
+export type TBackgroundColor = {
+  type: "solid-color";
+} & Color;
+
+export type TBackgroundGradient = {
+  type: "gradient";
+} & Gradient;
+
+export type TBackgroundImage = {
+  type: "image";
+} & ImageManifest;
+
+/**
+ * @deprecated - this will be removed
+ */
+export type TBackgroundGraphics = {
+  type: "graphics";
+};
 
 export type BackgroundPaintLike =
-    | (Color & {
-          type: "solid-color";
-      })
-    | (Gradient & {
-          type: "gradient";
-      })
-    | { type: "graphics" };
+  | TBackgroundColor
+  | TBackgroundGradient
+  | TBackgroundImage
+  | TBackgroundGraphics;
+
+export type BackgroundType = BackgroundPaintLike["type"];
+
 export type Background = BackgroundPaintLike | BackgroundPaintLike[];
